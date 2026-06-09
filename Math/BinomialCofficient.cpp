6d039a541a21f06043f098e94e8ad7fddd010f2f
Ishika-om19 <somi37987@gmail.com>
2026-06-09 20:44:57 +0530

    Bionomial Cofficient

---
+++ Math/BinomialCofficient.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n ){
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact*=i;
+    }
+    return fact;
+}
+
+int binomialCofficient(int n,int r){
+    int val1=factorial(n);
+    int val2=factorial(r);
+    int val3=factorial(n-r);
+    int result=val1/(val2*val3);
+    return result;
+}
+
+int main(){
+    int n,r;
+    cout<<"Enter the value of n :";
+    cin>>n;
+    cout<<"Enter the value of r :";
+    cin>>r;
+    cout<<"Binomial Coefficient is:"<<binomialCofficient(n,r)<<endl;
+    return 0;
+}
