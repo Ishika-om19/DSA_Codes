95af8fdab5b5a3b520cbed0e89f5a158221ecbfc
Ishika-om19 <somi37987@gmail.com>
2026-06-18 23:41:01 +0530

    Using Kadan's Algorithm Finding Subarray Sum

---
+++ Array/KadansAlgoMaxSubarraySum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+#include<algorithm>
+using namespace std;
+
+void maxSubarraySum(int *arr, int n){
+    int maxSum=INT_MIN;
+    int currSum=0;
+    for(int i=0;i<n;i++){
+        currSum += arr[i];
+        maxSum=max(currSum, maxSum);
+        if(currSum<0){
+            currSum=0;
+        }
+    }
+    cout<<"maximum Subarray Sum = "<<maxSum<<endl;
+}
+int main(){
+    int arr[6]={2,-3,6,-5,4,2};
+    int n= sizeof(arr)/sizeof(int);
+    maxSubarraySum(arr,n);
+    return 0;
+}
