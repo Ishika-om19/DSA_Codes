b6736085c6be0c2ef7118be6a64151b2fae3e171
Ishika-om19 <somi37987@gmail.com>
2026-06-21 23:59:37 +0530

    Bubble Sort

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void print(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void bubbleSort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        bool isSwap=false;
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                isSwap=true;
+            }
+        }
+            if(!isSwap){
+                break;
+            }
+        }
+            print(arr, n);
+}
+
+int main(){
+    int arr[5]={5,4,1,3,2};
+
+    bubbleSort(arr, 5);
+    return 0;
+}
