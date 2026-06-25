cfdcf5b4c9eea619b7c80414c7031fda83643d62
Ishika-om19 <somi37987@gmail.com>
2026-06-26 00:07:57 +0530

    Spiral Matrix

---
+++ 2DArray/SpiralMatrix.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void spiralMatrix(int mat[][4], int n, int m){
+    int srow=0, scol=0;
+    int erow=n-1, ecol=m-1;
+
+    //Condition for Odd Matrix
+    while(srow<=erow && scol<=ecol){
+        //Top
+        for(int j=scol; j<=ecol; j++){
+            cout<<mat[srow][j]<<" ";
+        }
+        //Right
+        for(int i=srow+1; i<=erow; i++){
+            cout<<mat[i][ecol]<<" ";
+        }
+        //Bottom
+        for(int j=ecol-1; j>=scol; j--){
+            if(srow == erow){
+                break;
+            }
+            cout<<mat[erow][j]<<" ";
+        }
+        //Left
+        for(int i=erow-1; i>=srow+1; i--){
+            if(scol==ecol){
+                break;
+            }
+            cout<<mat[i][scol]<<" ";
+        }
+        srow++; scol++;
+        erow--; ecol--;
+    }
+    cout<<endl;
+}
+
+int main(){
+    int matrix[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    spiralMatrix(matrix, 4, 4);
+    return 0;
+}
