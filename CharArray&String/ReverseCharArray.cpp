7ea990b93e7e3ab062c4fdccb00761f8d843f669
Ishika-om19 <somi37987@gmail.com>
2026-07-05 00:19:15 +0530

    Reverse Character Array

---
+++ CharArray&String/ReverseCharArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void reverse (char word[], int n){
+    int start=0, end=n-1;
+    while(start < end){
+        swap(word[start], word[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    char word[] = "Hello";
+    reverse(word, strlen(word));
+    cout <<"Reversed word is : "<< word << endl;
+    return 0;
+}
