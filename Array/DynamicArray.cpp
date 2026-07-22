f41c40b3b43fc5625f6c14eef429a8c213ac701f
Ishika-om19 <somi37987@gmail.com>
2026-07-23 00:22:02 +0530

    Dynamic Allocation

--- Array/DynamicArray.cpp
+++ Array/DynamicArray.cpp
@@ -4,7 +4,7 @@ int main(){
     int size;
     cin>> size;
     int *arr = new int[size];  //here dynamic array allocation using new in stact because in main function"int main()"
-                               //due to this in main function we don't have to reallocate using delete []arr
+                               //due to this in main function we don't have to deallocate using delete []arr
                                //because this not in heap it is in stack   
     int x=1;
     for(int i=0; i<size; i++){
