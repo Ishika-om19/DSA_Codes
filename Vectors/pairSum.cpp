c44d151913063265158d56df8497295e3c2d8393
Ishika-om19 <somi37987@gmail.com>
2026-08-05 19:32:22 +0530

    Pair Sum in shorted vector

---
+++ Vectors/pairSum.cpp
@@ -0,0 +1,32 @@
+# include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> pairSum(vector<int> arr , int target){
+    int st = 0, end = arr.size()-1;
+    int currSum = 0;
+    vector<int> ans;
+
+    while(st < end){
+        currSum = arr[st] + arr[end];
+
+        if(currSum == target){
+            ans.push_back(st);
+            ans.push_back(end);
+            return ans;
+        } else if(currSum > target){
+            end--;
+        } else{
+            st++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr ={1, 2, 3, 5, 6, 7, 9};
+    int target = 6;
+    vector<int> ans = pairSum(arr, target);
+    cout << ans[0] << " " << ans[1] << endl;
+    return 0;
+}
